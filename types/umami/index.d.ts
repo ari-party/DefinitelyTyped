6ec2093315d10e0d03e8c8024c6c107a2d16678d
astrid <git@astrid.email>
2025-01-29 14:50:36 +0100

    [@types/umami] update to ^2.13

--- types/umami/index.d.ts
+++ types/umami/index.d.ts
@@ -1,29 +1,48 @@
 declare var umami: umami.umami;
 
+type WithRequired<T, K extends keyof T> = T & { [P in K]-?: T[P] };
+
 /**
  * @see {@link https://umami.is/docs/tracker-functions|Umami Docs}
  */
 declare namespace umami {
+    interface PageViewProperties {
+        website: string;
+        hostname: string;
+        language: string;
+        referrer: string;
+        screen: string;
+        title: string;
+        url: string;
+    }
+
+    interface EventData {
+        [key: string]:
+            | number
+            | string
+            | EventData
+            | number[]
+            | string[]
+            | EventData[];
+    }
+
+    interface CustomPayload
+        extends WithRequired<Partial<PageViewProperties>, "website"> {
+        name?: string;
+        data?: EventData;
+    }
+
     interface umami {
-        track(): Promise<string> | undefined;
-        track(
-            event_name: string,
-            event_data?: { [key: string]: any },
-        ): Promise<string> | undefined;
-        track(custom_payload: {
-            website: string;
-            [key: string]: any;
-        }): Promise<string> | undefined;
-        track(
-            callback: (props: {
-                hostname: string;
-                language: string;
-                referrer: string;
-                screen: string;
-                title: string;
-                url: string;
-                website: string;
-            }) => { website: string; [key: string]: any },
-        ): Promise<string> | undefined;
+        track: {
+            (): Promise<string>;
+            (eventName: string): Promise<string>;
+            (eventName: string, eventData: EventData): Promise<string>;
+            (props: CustomPayload): Promise<string>;
+            (
+                callback: (props: PageViewProperties) => CustomPayload,
+            ): Promise<string>;
+        };
+
+        identify: (data: EventData) => Promise<string>;
     }
 }
